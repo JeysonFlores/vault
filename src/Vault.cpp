407cca202ee35ef421ef18af16755e316500daac
JeysonFlores <jeyson.flores@protonmail.com>
2022-02-28 11:44:54 -0700

    Project expanded

--- src/Vault.cpp
+++ src/Vault.cpp
@@ -14,17 +14,15 @@ void Vault::Application::on_prefers_color_scheme_changed()
 {
     auto granite_settings = granite_settings_get_default();
     auto gtk_settings = Gtk::Settings::get_default();
-    auto colour_scheme = granite_settings_get_prefers_color_scheme(granite_settings);
+    auto color_scheme = granite_settings_get_prefers_color_scheme(granite_settings);
 
-    gtk_settings->property_gtk_application_prefer_dark_theme().set_value(colour_scheme == GRANITE_SETTINGS_COLOR_SCHEME_DARK);
+    gtk_settings->property_gtk_application_prefer_dark_theme().set_value(color_scheme == GRANITE_SETTINGS_COLOR_SCHEME_DARK);
 }
 
 void Vault::Application::on_activate()
 {
-    // Initially set the preferred colour scheme
     on_prefers_color_scheme_changed();
 
-    // Listen for changes to the colour scheme
     auto granite_settings = granite_settings_get_default();
     g_signal_connect(granite_settings, "notify::prefers-color-scheme", G_CALLBACK(&on_prefers_color_scheme_changed), NULL);
 
@@ -40,4 +38,10 @@ void Vault::Application::on_activate()
 
     app_window->resize(window_width, window_height);
     app_window->move(pos_x, pos_y);
+}
+
+int main(int argc, char* argv[])
+{
+    auto application = Vault::Application::create();
+    return application->run(argc, argv);
 }
